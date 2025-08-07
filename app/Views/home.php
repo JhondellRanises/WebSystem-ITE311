0b24e2505eb60f09bb5697f769f68a2b31b83c20
JhondellRanises <projhondell@gmail.com>
2025-08-08 00:03:50 +0800

    Added Bootstrap template and Home controller

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,2 +1,7 @@
-<h1>Welcome!</h1>
-<p>This is the homepage of WebSystem-ITE311 with Bootstrap layout.</p>
+<?= $this->extend('template') ?>
+
+<?= $this->section('content') ?>
+    <h1>Welcome!</h1>
+    <p>This is the homepage of WebSystem-ITE311 with Bootstrap layout.</p>
+<?= $this->endSection() ?>
+    
